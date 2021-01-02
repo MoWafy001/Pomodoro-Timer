82631c0d50d38cd8520d2681344dbfb61e696400
Mohamed Wafy <wafy123445@gmail.com>
2021-01-03 01:39:31 +0200

    ok

--- public/app.js
+++ public/app.js
@@ -1,19 +1,73 @@
-var work, shortBreak, sessions, longBreak;
+var work, shortBreak, sessions, sessionCounter=1, longBreak;
+var time, timer;
+var working = true;
 var errorBox = document.querySelector("#errorBox");
 
+function setTime(seconds) {
+    let s = ""+(seconds-Math.floor(seconds/60)*60);
+    if (s.length<2) {
+        s=""+"0"+s;
+    }
+    if (s.length<2) {
+        s=""+"0"+s;
+    }
+    let m = ""+Math.floor(seconds/60);
+    if (m.length<2) {
+        m="0"+m;
+    }
+    if (m.length<2) {
+        m="0"+m;
+    }
+    return m+":"+s;
+}
+clock = ()=>{
+    document.querySelector("#time").textContent = setTime(time);
+    if(time>0) {
+        time-=1;   
+    }else{
+        if (working) {
+            working=false
+            if (sessionCounter<sessions || sessions == 1) {
+                if (sessions!=1 && longBreak!=0) {
+                    sessionCounter+=1   
+                }
+                time = shortBreak
+                document.querySelector("#sessionType").textContent="short break";
+            }else{
+                document.querySelector("#sessionType").textContent="long break";
+                sessionCounter=1
+                time = longBreak
+            }
+        }else{
+            if (sessions!=1 && longBreak!=0) {
+                document.querySelector("#sessionType").innerHTML=`work ${sessionCounter}/${sessions}`;
+            }else{
+                document.querySelector("#sessionType").innerHTML=`work`;
+            }
+            
+            working=true
+            time = work
+        }
+    }
+}
+function run() {
+    timer = setInterval(clock,1000)
+}
+
 function start() {
+    //This part validates the user's input
     ok = true;
     for(const i of document.querySelectorAll("#setup p input")){
         ok = i.checkValidity();
         if(!ok){
             break;
         }
-    }
+    }// If everything is OK, the user's input is then stored in variables. Next, transition to the timer.
     if(ok){
-        work = document.querySelector("#work").value;
-        shortBreak = document.querySelector("#shortBreak").value;
+        work = document.querySelector("#work").value * 60;
+        shortBreak = document.querySelector("#shortBreak").value * 60;
         sessions = document.querySelector("#sessions").value;
-        longBreak = document.querySelector("#longBreak").value;
+        longBreak = document.querySelector("#longBreak").value * 60;
         document.querySelector("#setup").style.transform = "translateY(-130%) scale(0)";
         setTimeout(()=>{
             document.querySelector("#setup").style.display = "none";
@@ -22,7 +76,14 @@ function start() {
         setTimeout(()=>{
             document.querySelector("#running").style.transform = "translateY(0) scale(1)";
         },1100);
-    }else{
+        time = work;
+        if (sessions!=1 && longBreak!=0) {
+            document.querySelector("#sessionType").innerHTML=`work ${sessionCounter}/${sessions}`;
+        }else{
+            document.querySelector("#sessionType").innerHTML=`work`;
+        }
+        run();
+    }else{// If everything isn't ok, display an error message.
         errorBox.style.opacity="1";
         errorBox.style.top="100%";
         setTimeout(()=>{errorBox.style.opacity="0";errorBox.style.top="80%";},3000)
